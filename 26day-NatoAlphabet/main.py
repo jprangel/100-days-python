fbba5c0106916b9ce6406d0d74aa8dfe716840e2
Jessica Rangel <jessica.rangel@workday.com>
2023-02-10 15:11:16 +0100

    Adding try except concept

--- 26day-NatoAlphabet/main.py
+++ 26day-NatoAlphabet/main.py
@@ -2,6 +2,15 @@ import pandas
 
 nato = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet_dict = {row.letter:row.code for index,row in nato.iterrows()}
-word = input("Enter a word: ").upper()
-nato_word = [nato_alphabet_dict[l] for l in word]
-print(nato_word)
+
+def generate_phonetic_word():
+    word = input("Enter a word: ").upper()
+    try:
+        nato_word = [nato_alphabet_dict[l] for l in word]
+    except KeyError:
+        print(f"Sorry {word} is invalid, only letter in the latin alphabet are allowed")
+        generate_phonetic_word()
+    else:
+        print(nato_word)
+
+generate_phonetic_word()
